31ce4d852277494fa39ea2748984ef26feb666e0
nomaanalii <nomaanali471@gmail.com>
2024-12-05 14:46:14 +0530

    last changes

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ print("my first project will be sentiment analysis bot and web scaper  ")
 print("both of the project are my sih projects ")
 int a = 5 
 print ( a) 
+""" code for making a calculator """
+int = a ( input (enter the first number :))
+int = b ( input (enter the second number :))
+int sum = a +b 
+print ( sum )
 
 
