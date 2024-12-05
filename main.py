9d7f2c0a680672695e888b5c16164dc081a77318
nomaanalii <nomaanali471@gmail.com>
2024-12-05 14:28:35 +0530

    frist changes

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Hello world ')
-print("my first project will be sentiment analysis bot ")
+print("my first project will be sentiment analysis bot and web scaper  ")
+print("both of the project are my sih projects ")
+int a = 5 
+print ( a) 
+
 
